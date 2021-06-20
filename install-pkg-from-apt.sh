0471d8f37db85ece8e828e5e65aa248ad3e791b3
raja-ashok <rashok.svks@gmail.com>
2021-06-20 12:33:59 +0530

    Apt pkg install

---
+++ install-pkg-from-apt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt install make gcc g++
+apt install net-tools
+
+# Install python2.7
+apt install python
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python get-pip.py
+
+# Install python3
+apt install python3 python3-pip
+
+# Tools
+apt install gnome-tweak-tool
+
