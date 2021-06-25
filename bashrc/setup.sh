88daa022a4ce73b61c53267d8629aaf374289d59
raja-ashok <raja.ashok@fungible.com>
2021-06-25 15:42:57 +0530

    git aliases

--- bashrc/setup.sh
+++ bashrc/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cat ./bashrc >> ~/.bashrc
-cp ./bashrc-extra ~/.bashrc-extra
-cp ./bashrc-aliases ~/.bashrc-aliases
-cp ./bashrc-additional ~/.bashrc-additional
-cp ./bashrc-additional-work ~/.bashrc-additional-work
+cat ./bashrc/bashrc >> ~/.bashrc
+cp ./bashrc/bashrc-extra ~/.bashrc-extra
+cp ./bashrc/bashrc-aliases ~/.bashrc-aliases
+cp ./bashrc/bashrc-additional ~/.bashrc-additional
+cp ./bashrc/bashrc-additional-work ~/.bashrc-additional-work
