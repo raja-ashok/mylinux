bc78017bb8b4432780bddcecb007ab7eb847ce0b
raja-ashok <rashok.svks@gmail.com>
2021-06-27 17:07:50 +0530

    Add Myhome to bashrc

--- bashrc/setup.sh
+++ bashrc/setup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-cat ./bashrc/bashrc >> ~/.bashrc
-cp ./bashrc/bashrc-extra ~/.bashrc-extra
-cp ./bashrc/bashrc-aliases ~/.bashrc-aliases
-cp ./bashrc/bashrc-additional ~/.bashrc-additional
-cp ./bashrc/bashrc-additional-work ~/.bashrc-additional-work
+if [ -z ${MYHOME} ]; then
+    export MYHOME=$HOME
+else
+    export MYHOME=$MYHOME
+fi
+echo "Copying bashrcs to ${MYHOME}"
+if grep "source ~/.bashrc-extra" ~/.bashrc
+then
+    echo "bashrc no need update"
+else
+    cat ./bashrc/bashrc >> ${MYHOME}/.bashrc
+fi
+
+echo "export MYHOME=$MYHOME" > ${MYHOME}/.bashrc-extra
+cat ./bashrc/bashrc-extra >> ${MYHOME}/.bashrc-extra
+cp ./bashrc/bashrc-aliases ${MYHOME}/.bashrc-aliases
+cp ./bashrc/bashrc-additional ${MYHOME}/.bashrc-additional
+cp ./bashrc/bashrc-additional-work ${MYHOME}/.bashrc-additional-work
