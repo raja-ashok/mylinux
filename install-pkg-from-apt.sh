43349713258cc82dc64c6413ed858484b6cd9b3d
raja-ashok <rashok.svks@gmail.com>
2021-06-30 21:12:48 +0530

    Install tree cmd

--- install-pkg-from-apt.sh
+++ install-pkg-from-apt.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-apt install make gcc g++
-apt install net-tools
-apt install cloc
+apt install make gcc g++ -y
+apt install net-tools -y
+apt install cloc -y
 
 # Install python2.7
-apt install python
+apt install python -y
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 python get-pip.py
 
 # Install python3
-apt install python3 python3-pip
+apt install python3 python3-pip -y
 
 # Tools
-apt install gnome-tweak-tool
+apt install gnome-tweak-tool -y
 
 # Doc
-apt install meld
-apt install hplip
-apt install jpegoptim
-apt install imagemagick
+apt install meld -y
+apt install hplip -y
+apt install jpegoptim -y
+apt install imagemagick -y
+
+# Utils
+apt install tree -y
