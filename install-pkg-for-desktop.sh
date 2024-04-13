da20a8825c51ddc19ce9a9fa89c5c245ca587284
Raja Ashok Karmegam <rashok.svks@gmail.com>
2024-04-13 21:10:23 +0530

    gnome tweak pkg name is different for ubuntu 22.04

--- install-pkg-for-desktop.sh
+++ install-pkg-for-desktop.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+source /etc/lsb-release
+
+if [ -z $DISTRIB_RELEASE ] || [ -z $DISTRIB_ID ]; then
+    echo "DISTRIB_RELEASE and DISTRIB_ID are not set"
+else
+    if [ $DISTRIB_ID == "Ubuntu" ]; then
+        IFS='.' read -r -a distro_vers <<< $DISTRIB_RELEASE
+        major_ver=${distro_vers[0]}
+    fi
+fi
+
 sudo apt update
 
 # Tools
 sudo apt install tilix -y
-sudo apt install gnome-tweak-tool -y
+# Install gnome tweak
+if [ -z $major_ver ]; then
+    echo "Major release version parsing failed"
+else
+    if [ $(($major_ver)) -ge 22 ]; then
+        sudo apt install gnome-tweaks -y
+    else
+        sudo apt install gnome-tweak-tool -y
+    fi
+fi
 sudo apt install gnome-shell-extensions -y
 sudo apt install chrome-gnome-shell -y
 sudo apt install sshpass -y
