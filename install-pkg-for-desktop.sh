fe2608bbd88db38f56367d8f6cff55d15c704f79
raja-ashok <rashok.svks@gmail.com>
2025-06-04 19:51:03 +0530

    Updates to pkg installation

--- install-pkg-for-desktop.sh
+++ install-pkg-for-desktop.sh
@@ -56,6 +56,5 @@ sudo apt install libgl1-mesa-glx libegl1-mesa libxcb-xtest0 -y
 
 # Install deb application
 # chrome browser
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+./install-chrome-browser.sh
+
